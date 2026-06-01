367bfa52bd678ea862a8620aba4ebbd7fc4bb207
Saleh7127 <abusalehjunior@gmail.com>
2026-06-02 00:15:35 +0300

    update the data for module 8

--- data/modules_ult/module_008.py
+++ data/modules_ult/module_008.py
@@ -1,16 +1,15 @@
-import random
+def horizontalMaxs(a):
+	max1, max2 = 0,0
+	posMax1, posMax2 = 0,0
+	for i in range(1,len(a)-1):
+		if a[i]>a[i+1] and a[i]>a[i-1]:
+			if a[i]>max1:
+				max1 = a[i]
+				posMax1 = i
+	for i in range(1,len(a)-1):
+		if a[i]>a[i+1] and a[i]>a[i-1]:		
+			if a[i]>max2 and a[i]<max1:
+				max2 = a[i]
+				posMax2 = i
 
-def create_three_random_v_indices(current_vector_index, numOfPop):
-    while True:
-        v_index_1 = random.randint(0,numOfPop)
-        if (v_index_1 != current_vector_index):
-            break
-    while True:
-        v_index_2 = random.randint(0, numOfPop)
-        if (v_index_2 != v_index_1 and v_index_2 != current_vector_index):
-            break
-    while True:
-        v_index_3 = random.randint(0, numOfPop)
-        if (v_index_3 != v_index_1 and v_index_3 != v_index_2 and v_index_3 != current_vector_index):
-            break
-    return v_index_1, v_index_2, v_index_3
+	return posMax1, max1, posMax2, max2
